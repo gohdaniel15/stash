57afceb39a39877e3344ee45b8ef56826757918f
Lydia Cheah <cheahlizuan@gmail.com>
2016-07-30 10:01:25 +0800

    Change root path and add welcome page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730011956) do
+ActiveRecord::Schema.define(version: 20160730014717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,9 @@ ActiveRecord::Schema.define(version: 20160730011956) do
     t.string   "uid"
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
     t.string   "avatar"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
