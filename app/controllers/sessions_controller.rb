7a19307f20488b506c80214ef77dd242203e7a5f
Lydia Cheah <cheahlizuan@gmail.com>
2016-07-30 11:46:48 +0800

    Omniauth Github set up and create sign in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,26 @@
 class SessionsController < ApplicationController
+	def new
+	end
+
 	def create
+		@user = User.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid]) || User.create_with_omniauth(auth_hash)
+	  if @user
+     session[:user_id] = @user.id
+     redirect_to root_url
+	  else
+     redirect_to root_url
+	  end
 	end
 
 	def destroy
+		byebug
 		session[:user_id] = nil
-    redirect_to root_url
+    redirect_to sign_in_path
 	end
+
+	protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 	root 'users#welcome'
 
-	get "/auth/:provider/callback" => "sessions#create"
+	get "/auth/github/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", as: "sign_out"
+  get "/signin" => "sessions#new", as: "sign_in"
 
-  resources :users, only: [:new, :create, :destroy]
+  resources :users, only: [:welcome, :new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
